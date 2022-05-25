b80df3f6de4d96bc6d50a81d208a69d8702cb577
Adrenaline444 <xc2491919548@gmail.com>
2022-05-25 09:19:20 +0800

    123

--- vue5/vue5/src/main.js
+++ vue5/vue5/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import Vant from 'vant';
 import 'vant/lib/index.css';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 Vue.use(Vant);
